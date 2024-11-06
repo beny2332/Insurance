d4f64a659d98e6a9519c8d219e5fb0ce384da5cf
BinyaminYacovson <bj57577@gmail.com>
2024-11-06 10:31:12 +0200

    add basic routing for claims and policies pages; integrate BrowserRouter in main entry

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Claims from './claims/Claims'
+import Policies from './policies/Policies'
+import PolicyDetails from './policies/PolicyDetails'
+import ClaimDetails from './claims/ClaimDetails'
 
 export default function Page() {
   return (
     <div className='full'>
-      Page
+      <Routes>
+        <Route path='/' element={<Navigate replace to="/policies"/>}/>
+        <Route path='claims' element={<Claims/>}/>
+        <Route path='claims/:id' element={<ClaimDetails/>}/>
+        <Route path='policies' element={<Policies/>}/>
+        <Route path='policies/:id' element={<PolicyDetails/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/pages/policies/Policies.tsx
+++ src/components/pages/policies/Policies.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Policies() {
   return (
     <div>
-      
+      Policies
     </div>
   )
 }
