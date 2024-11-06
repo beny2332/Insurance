4fb794c612b0da22ae3e72054dd23a40fc2e31ec
BinyaminYacovson <bj57577@gmail.com>
2024-11-06 09:40:34 +0200

    integrate Redux for state management and display treasure data in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addNewPolicy, approveClaim } from './redux/store'
 
 export default function App() {
+  const treasure = useSelector((st:any)=>st.treasure)
+
   return (
     <div>
-      
+      {treasure}
     </div>
   )
 }
